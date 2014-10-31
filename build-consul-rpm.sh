1d62abd09e7ee02520e52d61759b90dee095aedc
Oliver Otzen <oliver.otzen@hypoport.de>
2014-10-31 09:13:28 +0100

    build file can now build rpm's for i386 and x86_64

--- build-consul-rpm.sh
+++ build-consul-rpm.sh
@@ -2,35 +2,58 @@
 #
 
 if [[ -z "$1" ]]; then
-  echo "Usage: $0 <VERSION>"
+  echo $"Usage: $0 <VERSION> [ARCH]"
   exit 1
 fi
 
 VERSION=$1
-URL="https://dl.bintray.com/mitchellh/consul/${VERSION}_linux_amd64.zip"
-echo "Creating consul RPM build file version ${VERSION}"
+
+if [[ -z "$2" ]]; then
+  ARCH=`uname -m`
+else
+  ARCH=$2
+fi
+
+ZIP=${VERSION}_linux_amd64.zip
+
+case "${ARCH}" in
+    i386)
+        ZIP=${VERSION}_linux_386.zip
+        ;;
+    x86_64)
+       ZIP=${VERSION}_linux_amd64.zip
+        ;;
+    *)
+        echo $"Unknown architecture ${ARCH}" >&2
+        exit 1
+        ;;
+esac
+
+URL="https://dl.bintray.com/mitchellh/consul/${ZIP}"
+echo $"Creating consul ${ARCH} RPM build file version ${VERSION}"
 
 # fetching consul
 wget --no-check-certificate -q $URL  || {
-    echo "URL or version not found!" >&2
+    echo $"URL or version not found!" >&2
     exit 1
 }
 
 # create target structure
-rm -rf target
+rm -rf target/usr target/etc
 mkdir -p target/usr/local/bin
 mkdir -p target/etc/init.d
 cp -r sources/etc/ target/
 
 # unzip
 unzip -qq ${VERSION}_linux_amd64.zip -d target/usr/local/bin/
-rm ${VERSION}_linux_amd64.zip
+rm ${ZIP}
 
 # create rpm
 fpm -s dir -t rpm \
        -C target -n consul \
        -v ${VERSION} \
        -p target \
+       -a ${ARCH} \
        --after-install spec/service_install.spec \
        --after-remove spec/service_uninstall.spec \
        --description "Consul RPM package for RedHat Enterprise Linux 6" \
